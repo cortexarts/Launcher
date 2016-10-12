8892ef51c23940e22a03157b1ea83f26a46cf7c0
JasperDre <j.drescher@cortexarts.com>
2016-10-12 18:59:11 +0200

    [Updated] Alpha v0.1
    Added XML template and simple back-end

---
+++ Launcher/Launcher/Launcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Launcher
+{
+    class LauncherBackEnd
+    {
+        public static void PlayGame()
+        {
+            Process.Start("test.txt");
+        }
+
+        public static void LaunchWebsite(string url)
+        {
+            Process.Start(url);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Launcher
-Launcher interface
+[Launcher](https://github.com/cortexarts/Launcher)
+==================================================
+
+Simple game launcher written in C#.
+
+Project made by [JasperDre](https://github.com/JasperDre)
